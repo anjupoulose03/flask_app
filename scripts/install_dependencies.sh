4463d97dd15cb50024b14f9caa3ba0854d6613a0
Anju Poulose <159629591+anjupoulose03@users.noreply.github.com>
2025-08-21 20:12:48 -0400

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+set -e
+
 cd /home/ubuntu/flask-ci-cd-app
+
+# Update and install Python packages
 sudo apt-get update -y
 sudo apt-get install -y python3-pip python3-venv
-pip3 install -r requirements.txt
+
+# Create virtual environment if it doesn't exist
+python3 -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install dependencies inside venv
+pip install -r requirements.txt
